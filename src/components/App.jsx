b6d987e7605350ca9b57aa1f721bbdc2eb81121d
Cyrus Ducut <cyrus.ducut@fieldforce4.com>
2024-07-08 00:27:36 +0800

    goit-react-hw-05-movies

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import SharedLayout from './Shared/SharedLayout';
 import { NotFoundPage } from '../pages/NotFoundPage';
 import { lazy, Suspense } from 'react';
@@ -26,7 +26,7 @@ export const App = () => {
               <Route path="reviews" element={<ReviewList />} />
             </Route>
           </Route>
-          <Route path="*" element={<NotFoundPage />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
    </Suspense>
   );
